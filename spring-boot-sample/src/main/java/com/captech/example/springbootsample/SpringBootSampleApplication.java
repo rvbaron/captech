aa4e4ce55f0a391d1fdf4b83c06043239eaa7d40
rbaron <rbaron@captechconsulting.com>
2026-09-03 15:41:16 -0500

    [initial] spring-boot-sample

---
+++ spring-boot-sample/src/main/java/com/captech/example/springbootsample/SpringBootSampleApplication.java
@@ -0,0 +1,13 @@
+package com.captech.example.springbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootSampleApplication.class, args);
+    }
+
+}
